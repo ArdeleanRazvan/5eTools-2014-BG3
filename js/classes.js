bcd55df73f98f4757e984ec6d7b45a6fe5d01e4f
Razvan Ardelean <ardelean.razvan.c@gmail.com>
2025-08-19 22:20:39 +0300

    removed  title from table

--- js/classes.js
+++ js/classes.js
@@ -1192,9 +1192,7 @@ class ClassesPage extends MixinComponentGlobalState(MixinBaseComponent(MixinProx
 		<div class="class-title"><h2 class="ve-text-left cls-tbl__disp-name">The ${cls.name} Table</h2><span class="scroll-right"><img src="./img/svgs/mouse.png" height="16"/>Scroll Right</span></div>
 		<table class="cls-tbl shadow-big w-100 mb-2">
 			<tbody>
-			<tr><th class="ve-tbl-border" colspan="999"></th></tr>
-			<tr><th class="ve-text-left cls-tbl__disp-name" colspan="999">${cls.name}</th></tr>
-			<tr>
+		<tr>
 				<th colspan="3"></th> <!-- spacer to match the 3 default cols (level, prof, features) -->
 				${$tblGroupHeaders}
 			</tr>
